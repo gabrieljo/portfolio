3cfe446293f834b2b8bef7d4dfcb6f2d70ed0d02
Gabriel <gabrieljo1302@gmail.com>
2021-10-11 21:25:15 +1300

    add event to contact btn

--- main.js
+++ main.js
@@ -10,12 +10,17 @@ document.addEventListener('scroll', () => {
   }
 })
 
+function moveScrollToElement(event) {
+  const target = event.target
+  const link = target.dataset.link
+  if(link == null) return
+  const scrollTo = document.querySelector(link)
+  scrollTo.scrollIntoView({behavior:"smooth"})
+}
 
 // const navbarMenu = document.querySelector('.navbar__menu')
-// navbarMenu.addEventListener('click', (event) => {
-//   const target = event.target
-//   const link = target.dataset.link
-//   if(link == null) return
-//   const scrollTo = document.querySelector(link)
-//   scrollTo.scrollIntoView({behavior:"smooth"})
-// })
+// navbarMenu.addEventListener('click', moveScrollToElement)
+
+const btnContact = document.querySelector('.home__contact')
+btnContact.addEventListener('click', moveScrollToElement)
+
