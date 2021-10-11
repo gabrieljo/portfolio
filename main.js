de0de633de54cc2736f91d582cdd56ee4783a438
Gabriel <gabrieljo1302@gmail.com>
2021-10-11 21:02:57 +1300

    navbar scripting

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const navBar = document.querySelector('#navbar');
+const navBarHeight = navBar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  if(window.scrollY > navBarHeight) {
+    navBar.classList.add('navbar--dark')
+  } else {
+    navBar.classList.remove('navbar--dark')
+  }
+})
