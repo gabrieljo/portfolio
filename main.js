92286eca0a8677db5fb9b81c1513042315cb3e9a
Gabriel <gabrieljo1302@gmail.com>
2021-10-11 21:20:00 +1300

    navbar scrolling

--- main.js
+++ main.js
@@ -9,3 +9,13 @@ document.addEventListener('scroll', () => {
     navBar.classList.remove('navbar--dark')
   }
 })
+
+
+// const navbarMenu = document.querySelector('.navbar__menu')
+// navbarMenu.addEventListener('click', (event) => {
+//   const target = event.target
+//   const link = target.dataset.link
+//   if(link == null) return
+//   const scrollTo = document.querySelector(link)
+//   scrollTo.scrollIntoView({behavior:"smooth"})
+// })
